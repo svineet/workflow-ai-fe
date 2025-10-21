1191e7ebb2146e2084b1e6cad27f3b6e780a12a3
Sukuru Sai Vineet <saivineet89@gmail.com>
2025-10-20 21:31:24 -0700

    selector for account in google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,9 @@ export async function loginWithGoogle() {
     provider: 'google',
     options: {
       redirectTo: `${window.location.origin}/auth/callback`,
+      queryParams: {
+        prompt: 'select_account',
+      },
     },
   })
   if (error) throw error
